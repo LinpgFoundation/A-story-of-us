49ebd98cef5d499ecb113fc3f71f797bd1108f27
yudonglin <yoshino1347716570@gmail.com>
2021-05-01 19:06:27 -0700

    twist the language
    I mean, I do not want to make a lot of changes to the original scripts. But I do want to twist them a little bit better.

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-#from linpgdev import linpg, pygame
-import linpg, pygame
+from linpgdev import linpg, pygame
+#import linpg, pygame
 
 # 对话系统
-def dialog(
-    screen: pygame.Surface,
-    chapterType: str,
-    chapterId: int,
-    part: str,
-    projectName: str = None
-    ) -> dict:
+def dialog(screen: pygame.Surface, chapterType: str, chapterId: int, part: str, projectName: str = None) -> dict:
     # 卸载音乐
     linpg.unloadBackgroundMusic()
     # 初始化对话系统模块
@@ -26,13 +20,7 @@ def dialog(
     return DIALOG.dialog_options
 
 # 对话编辑器
-def dialogEditor(
-    screen: pygame.Surface,
-    chapterType: str,
-    chapterId: int,
-    part: str,
-    projectName: str = None
-    ) -> None:
+def dialogEditor(screen: pygame.Surface, chapterType: str, chapterId: int, part: str, projectName: str = None) -> None:
     # 卸载音乐
     linpg.unloadBackgroundMusic()
     # 加载对话
@@ -44,25 +32,40 @@ def dialogEditor(
         linpg.display.flip()
 
 class MainMenu(linpg.SystemWithBackgroundMusic):
-    def __init__(self, screen):
+    def __init__(self, screen_size:tuple):
         # 初始化系统模块
         super().__init__()
         self.set_bgm(r"Assets/music/main_menu_bgm.ogg")
         # 加载主菜单背景
-        self.bg_img = linpg.loadImage(r"Assets/image/UI/bg0.png", (0, 0), screen.get_width(), screen.get_height())
+        self.bg_img = linpg.loadImage(r"Assets/image/UI/bg0.png", (0, 0), screen_size[0], screen_size[1])
         # 初始化返回菜单判定参数
         linpg.set_glob_value("BackToMainMenu", False)
         self.set_bgm_volume(linpg.get_setting("Sound", "background_music") / 100)
+        # 开发者信息
         self.show_developer_info_button = linpg.loadButton(
             r"Assets/image/UI/important.png",
-            (screen.get_width() * 0.9, screen.get_height() * 0.05),
-            (screen.get_height() * 0.05, screen.get_height() * 0.05),
+            (screen_size[0] * 0.9, screen_size[1] * 0.05),
+            (screen_size[1] * 0.05, screen_size[1] * 0.05),
             200
             )
-        self.developer_info = linpg.converter.generate_container(linpg.loadConfig(r"Data/ui.yaml", "developer_info"))
-    def draw(self, screen):
+        self.developer_info = linpg.converter.generate_ui(linpg.loadConfig(r"Data/ui.yaml", "developer_info"))
+        self.exit_button = linpg.loadButton(
+            r"Assets/image/UI/quit.png",
+            (screen_size[0] * 0.95, screen_size[1] * 0.05),
+            (screen_size[1] * 0.05, screen_size[1] * 0.05),
+            200
+            )
+        main_menu_txt = linpg.get_lang('MainMenu')
+        self.exit_confirm_menu = linpg.Message(
+            main_menu_txt["other"]["tip"], main_menu_txt["other"]["exit_confirm"],
+            (main_menu_txt["other"]["confirm"],main_menu_txt["other"]["deny"]), True, return_button=1, escape_button=1
+            )
+    def draw(self, screen:pygame.Surface) -> None:
+        # 确认背景音乐在播放
         self.play_bgm()
+        # 画出背景图片
         self.bg_img.draw(screen)
+        #处理输入
         for event in linpg.controller.events:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if not self.developer_info.hidden:
@@ -70,14 +73,17 @@ class MainMenu(linpg.SystemWithBackgroundMusic):
                         self.developer_info.hidden = True
                 elif self.developer_info.hidden is True and self.show_developer_info_button.is_hover():
                     self.developer_info.hidden = False
+                elif linpg.isHover(self.exit_button):
+                    if self.exit_confirm_menu.draw() == 0:
+                        self.stop()
                 else:
                     dialog(screen, "main_chapter", 1, "dialog_example")
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
                 dialogEditor(screen, "main_chapter", 1, "dialog_example")
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                self.stop()
+        #画出ui
         self.show_developer_info_button.draw(screen)
         self.developer_info.draw(screen)
+        self.exit_button.draw(screen)
 
 # 是否启动游戏
 GAMESTART: bool = True
@@ -101,7 +107,7 @@ if GAMESTART is True and __name__ == "__main__":
         screen.get_height() * 0.7,
     )
     # 主菜单模块
-    mainMenu = MainMenu(screen)
+    mainMenu = MainMenu(screen.get_size())
     # 主循环
     while mainMenu.is_playing():
         mainMenu.draw(screen)
