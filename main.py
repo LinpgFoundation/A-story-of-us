a58219e81bc31538cc3d57318cef91d6243efaee
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2021-06-22 13:17:36 -0700

    [PATCH] linpg 3.1.0 api update (#2)
    * using upcoming 3.1.0 api
    * small bug fixing

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+#导入linpg引擎
 try:
     from linpgdev import linpg
-except BaseException:
+except Exception:
     import linpg
 
 # 对话系统
-def dialog(screen: linpg.ImageSurface, chapterType: str, chapterId: int, part: str, projectName: str = None) -> dict:
+def dialog(chapterType: str, chapterId: int, part: str, projectName: str = None) -> None:
     # 卸载音乐
-    linpg.unload_all_music()
+    linpg.media.unload()
     # 初始化对话系统模块
     DIALOG: linpg.DialogSystem = linpg.DialogSystem()
     if chapterType is None:
@@ -16,58 +17,57 @@ def dialog(screen: linpg.ImageSurface, chapterType: str, chapterId: int, part: s
     # DIALOG.auto_save = True
     # 主循环
     while DIALOG.is_playing():
-        DIALOG.draw(screen)
+        DIALOG.draw_on_screen()
         linpg.display.flip()
-    # 返回玩家做出的选项
-    return DIALOG.dialog_options
 
 # 对话编辑器
-def dialogEditor(screen: linpg.ImageSurface, chapterType: str, chapterId: int, part: str, projectName: str = None) -> None:
+def dialogEditor(chapterType: str, chapterId: int, part: str, projectName: str = None) -> None:
     # 卸载音乐
-    linpg.unload_all_music()
+    linpg.media.unload()
     # 加载对话
     DIALOG: linpg.DialogEditor = linpg.DialogEditor()
     DIALOG.load(chapterType, chapterId, part, projectName)
     # 主循环
     while DIALOG.is_playing():
-        DIALOG.draw(screen)
+        DIALOG.draw_on_screen()
         linpg.display.flip()
 
 #主菜单
 class MainMenu(linpg.SystemWithBackgroundMusic):
-    def __init__(self, screen_size:tuple):
+    def __init__(self):
         # 初始化系统模块
         super().__init__()
         self.set_bgm(r"Assets/music/main_menu_bgm.ogg")
         # 加载主菜单背景
-        self.bg_img = linpg.load_static_image(r"Assets/image/UI/bg0.png", (0, 0), screen_size[0], screen_size[1])
+        self.bg_img = linpg.load.static_image(r"Assets/image/UI/bg0.png", (0, 0), linpg.display.get_size())
         # 初始化返回菜单判定参数
-        linpg.set_glob_value("BackToMainMenu", False)
-        self.set_bgm_volume(linpg.get_setting("Sound", "background_music") / 100)
+        linpg.global_value.set("BackToMainMenu", False)
+        self.set_bgm_volume(linpg.setting.get("Sound", "background_music") / 100)
         # 开发者信息
-        self.show_developer_info_button = linpg.load_button(
+        self.show_developer_info_button = linpg.load.button(
             r"Assets/image/UI/important.png",
-            (screen_size[0] * 0.9, screen_size[1] * 0.05),
-            (screen_size[1] * 0.05, screen_size[1] * 0.05),
+            (linpg.display.get_width() * 0.9, linpg.display.get_height() * 0.05),
+            (linpg.display.get_height() * 0.05, linpg.display.get_height() * 0.05),
             200
             )
-        self.developer_info = linpg.converter.generate_ui(linpg.load_config(r"Data/ui.yaml", "developer_info"))
-        self.exit_button = linpg.load_button(
+        self.developer_info = linpg.converter.generate_ui(linpg.config.load(r"Data/ui.yaml", "developer_info"))
+        self.exit_button = linpg.load.button(
             r"Assets/image/UI/quit.png",
-            (screen_size[0] * 0.95, screen_size[1] * 0.05),
-            (screen_size[1] * 0.05, screen_size[1] * 0.05),
+            (linpg.display.get_width() * 0.95, linpg.display.get_height() * 0.05),
+            (linpg.display.get_height() * 0.05, linpg.display.get_height() * 0.05),
             200
             )
-        main_menu_txt = linpg.get_lang('MainMenu')
+        main_menu_txt = linpg.lang.get_text('MainMenu')
         self.exit_confirm_menu = linpg.Message(
             main_menu_txt["other"]["tip"], main_menu_txt["other"]["exit_confirm"],
-            (main_menu_txt["other"]["confirm"],main_menu_txt["other"]["deny"]), True, return_button=1, escape_button=1
+            (main_menu_txt["other"]["confirm"],main_menu_txt["other"]["deny"]),
+            True, return_button=1, escape_button=1
             )
-    def draw(self, screen: linpg.ImageSurface) -> None:
+    def draw_on_screen(self) -> None:
         # 确认背景音乐在播放
         self.play_bgm()
         # 画出背景图片
-        self.bg_img.draw(screen)
+        self.bg_img.draw_on_screen()
         #处理输入
         for event in linpg.controller.events:
             if event.type == linpg.MOUSE_BUTTON_DOWN and event.button == 1:
@@ -77,16 +77,16 @@ class MainMenu(linpg.SystemWithBackgroundMusic):
                 elif self.developer_info.hidden is True and self.show_developer_info_button.is_hover():
                     self.developer_info.hidden = False
                 elif linpg.is_hover(self.exit_button):
-                    if self.exit_confirm_menu.draw() == 0:
+                    if self.exit_confirm_menu.show() == 0:
                         self.stop()
                 else:
-                    dialog(screen, "main_chapter", 1, "dialog_example")
-            elif event.type == linpg.KEY.DOWN and event.unicode == "p":
-                dialogEditor(screen, "main_chapter", 1, "dialog_example")
+                    dialog("main_chapter", 1, "dialog_example")
+            elif event.type == linpg.key.DOWN and event.unicode == "p":
+                dialogEditor("main_chapter", 1, "dialog_example")
         #画出ui
-        self.show_developer_info_button.draw(screen)
-        self.developer_info.draw(screen)
-        self.exit_button.draw(screen)
+        self.show_developer_info_button.draw_on_screen()
+        self.developer_info.draw_on_screen()
+        self.exit_button.draw_on_screen()
 
 # 是否启动游戏
 GAMESTART: bool = True
@@ -94,22 +94,23 @@ GAMESTART: bool = True
 # 游戏主进程
 if GAMESTART is True and __name__ == "__main__":
     # 创建窗口
-    screen = linpg.display.init_screen()
+    linpg.display.init()
     # 窗口标题图标
     linpg.display.set_icon(r"Assets/image/UI/icon.png")
-    linpg.display.set_caption(linpg.get_lang("GameTitle"))
+    linpg.display.set_caption(linpg.lang.get_text("GameTitle"))
     # 初始化选项菜单
     linpg.init_option_menu(
-        screen.get_width() * 0.25,
-        screen.get_height() * 0.15,
-        screen.get_width() * 0.5,
-        screen.get_height() * 0.7
+        int(linpg.display.get_width() * 0.25),
+        int(linpg.display.get_height() * 0.15),
+        int(linpg.display.get_width() * 0.5),
+        int(linpg.display.get_height() * 0.7)
         )
     # 主菜单模块
-    mainMenu = MainMenu(screen.get_size())
+    mainMenu = MainMenu()
     # 主循环
     while mainMenu.is_playing():
-        mainMenu.draw(screen)
+        mainMenu.draw_on_screen()
         linpg.display.flip()
-    # 释放内容占用
-    linpg.display.quit()
+
+# 释放内容占用
+linpg.display.quit()
