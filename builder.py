d62a112556b36d9fd00b4a30bf63015d04684f8d
yudonglin <yoshino1347716570@gmail.com>
2021-08-30 19:56:28 -0700

    continue to remove unessential assets for upcoming linpg update

--- builder.py
+++ builder.py
@@ -11,6 +11,12 @@ if os.path.exists("dist"):
 # 打包main文件
 os.system("pyinstaller --noconsole main.spec")
 
+#重命名文件
+shutil.move(os.path.join("dist", "main"), os.path.join("dist", "A_story_of_us"))
+
+#删除opencv
+# shutil.rmtree(os.path.join("dist", "A_story_of_us", "cv2"))
+
 # 移除移除的缓存文件
 folders_need_remove: tuple[str] = ("build", "logs", "__pycache__", "crash_reports")
 for folder_p in folders_need_remove:
